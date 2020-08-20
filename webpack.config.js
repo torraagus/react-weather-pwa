730a1ba961704a9ab3680e19368fde08a189a7a2
Torra Agustin <torra.agustinignacio@gmail.com>
2020-08-20 17:44:58 -0300

    Changes to make PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development",
 
-  context: path.resolve(__dirname, 'src'), 
+  context: path.resolve(__dirname, "src"),
 
   entry: {
     app: "./index.js",
@@ -17,9 +18,9 @@ module.exports = {
   },
 
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 9000
+    port: 9000,
   },
 
   module: {
@@ -34,8 +35,32 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html")
-    })
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    // new ServiceWorkerWebpackPlugin({
+    //   entry: path.join(__dirname, "public", "serviceworker.js"),
+    //   filename: "serviceworker.js",
+    // }),
+    new CopyPlugin({
+      patterns: [
+        // {
+        //   from: path.join(__dirname, "public", "serviceworker.js"),
+        //   to: path.join(__dirname, "dist"),
+        // },
+        // {
+        //   from: path.join(__dirname, "public", "offline.html"),
+        //   to: path.join(__dirname, "dist"),
+        // },
+        // {
+        //   from: path.join(__dirname, "public", "manifest.json"),
+        //   to: path.join(__dirname, "dist"),
+        // },
+        {
+          from: path.join(__dirname, "public"),
+          to: path.join(__dirname, "dist"),
+        },
+      ],
+    }),
   ],
 
   devtool: "inline-source-map",
