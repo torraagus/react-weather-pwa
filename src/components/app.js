f52701c3afa0b75a4c2f2d944ece3932ea28c747
Torra Agustin <torra.agustinignacio@gmail.com>
2020-08-22 22:19:29 -0300

    Update file

--- src/components/app.js
+++ src/components/app.js
@@ -1,48 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { fetchWeather } from "../api/fetchWeather";
+import {
+  MainWrapper as Wrapper,
+  Celcius,
+  Country,
+  Description,
+  Image,
+  Input,
+  Location,
+  Name,
+  Search,
+  Temperature,
+  Text,
+  Weather,
+} from "./app.styles";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [error, setError] = useState(null);
   const [weather, setWeather] = useState({});
+  const queryRef = useRef(null);
 
   const search = async (e) => {
     if (e.key === "Enter") {
-      const data = await fetchWeather(query);
-      console.log(data);
-      setWeather(data);
+      const data = await fetchWeather(query).catch((err) => {
+        console.log(err);
+        setError(`${err}`);
+      });
+
+      if (data) {
+        setWeather(data);
+        setError(null);
+      }
+
       setQuery("");
     }
   };
 
+  useEffect(() => {
+    queryRef.current.focus();
+  }, []);
+
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
-      {weather.main && (
-        <div>
-          <h2>
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div>
+    <Wrapper>
+      <Search>
+        <Input
+          ref={queryRef}
+          type="text"
+          placeholder="Search for a city..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
+      </Search>
+      {error && <p>{error}</p>}
+      {weather?.main && (
+        <Weather>
+          <Location>
+            <Name>{weather.name}</Name>
+            <Country>{weather.sys.country}</Country>
+          </Location>
+          <Temperature>
             {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
-          </div>
-          <div>
-            <img
+            <Celcius>&deg;C</Celcius>
+          </Temperature>
+          <Description>
+            <Image
               src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
               alt={weather.weather[0].description}
             />
-            <p>{weather.weather[0].description}</p>
-          </div>
-        </div>
+            <Text>{weather.weather[0].description}</Text>
+          </Description>
+        </Weather>
       )}
-    </div>
+    </Wrapper>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./assets/favicon.ico";
+import "./assets/images/logo192.png";
+import "./assets/images/logo512.png";
+
+import "./index.css"
+
 import App from "./components/app";
 
-const app = <App />;
- 
-ReactDOM.render(app, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
