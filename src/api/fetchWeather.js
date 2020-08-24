df2a06de06053b1f4fa8e21b8217449cc5f7f7aa
Torra Agustin <torra.agustinignacio@gmail.com>
2020-08-24 07:29:36 -0300

    Change hardcoded api key for env variable

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,14 +1,13 @@
 import Axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const apiKey = "fcae7e2bb5a302ba98579155d04535c4";
 
 export const fetchWeather = async (query) => {
   const { data } = await Axios.get(URL, {
     params: {
       q: query,
       units: "metric",
-      APPID: apiKey,
+      APPID: process.env.WEATHER_API_KEY,
     },
   }).catch(() => {
     throw new Error("Cannot find the location!");
