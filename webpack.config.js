fa0a0e456b1d31c55137b6bb5fe273287071542f
Torra Agustin <torra.agustinignacio@gmail.com>
2020-08-24 07:30:12 -0300

    Update file for managing env. variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,92 @@
 const path = require("path"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   WorkboxPlugin = require("workbox-webpack-plugin"),
-  CopyPlugin = require("copy-webpack-plugin");
-  MiniCssExtractPlugin = require("mini-css-extract-plugin");
+  CopyPlugin = require("copy-webpack-plugin"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  webpack = require("webpack"),
+  dotenv = require("dotenv");
 
-module.exports = {
-  mode: "development",
+module.exports = () => {
+  const env = dotenv.config().parsed;
 
-  context: path.resolve(__dirname, "src"),
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
 
-  entry: {
-    app: "./index.js",
-  },
+  return {
+    mode: "development",
 
-  output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+    context: path.resolve(__dirname, "src"),
 
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000,
-  },
+    entry: {
+      app: "./index.js",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /node_modules,serviceworker/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.ico$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-        },
-      },
-      {
-        test: /\.(png|jpe?g|svg|gif)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-          outputPath: "images",
-          publicPath: "/images",
-        },
-      },
-      {
-				test: /\.(css|sass|scss)$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-			},
-    ],
-  },
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-    }),
-    new CopyPlugin({
-      patterns: [
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000,
+    },
+
+    module: {
+      rules: [
         {
-          from: path.join(__dirname, "public"),
-          to: path.join(__dirname, "dist"),
+          test: /\.js/,
+          exclude: /node_modules,serviceworker/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.ico$/,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+        {
+          test: /\.(png|jpe?g|svg|gif)$/,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "images",
+            publicPath: "/images",
+          },
+        },
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
       ],
-    }),
-    new WorkboxPlugin.InjectManifest({
-      swDest: "serviceworker.js",
-      swSrc: "./serviceworker.js",
-    }),
-  ],
+    },
 
-  // devtool: "inline-source-map",
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, "public"),
+            to: path.join(__dirname, "dist"),
+          },
+        ],
+      }),
+      new webpack.DefinePlugin(envKeys),
+      new WorkboxPlugin.InjectManifest({
+        swDest: "serviceworker.js",
+        swSrc: "./serviceworker.js",
+      }),
+    ],
+
+    // devtool: "inline-source-map",
+  };
 };
